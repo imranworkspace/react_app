82bf1dbea6c452e4c7f97405879d56c4b04d1409
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-14 20:00:08 +0530

    update using setState() with function in class based component

--- src/mycomponents/Class_Button_Event.js
+++ src/mycomponents/Class_Button_Event.js
@@ -16,10 +16,28 @@ class Class_Button_Event extends Component{
             this.setState({name:'imran'})
         }
 
+        // access state and props, update data using function or arrow fun
+        const Update_State_Click=()=>{
+            this.setState(function(state,props){
+                console.log('before update')
+                console.log('state ',state)
+                console.log('props ',props)
+                console.log('----------------')
+                console.log('after update')
+                console.log('state ',state)
+                console.log('props ',props)
+                this.setState({name:'afeen',age:27})
+
+            })
+        }
+        
+
         return (
             <div>
                 <h1>your name is<i> {this.state.name} </i> and your age is <i>{this.state.age}</i></h1>
-                <button onClick={handleClick} >Class Click</button>
+                <button onClick={handleClick} >Class Click</button> <br></br>
+                <button onClick={Update_State_Click} >Update by function Click</button>
+
             </div>
         )
     }
